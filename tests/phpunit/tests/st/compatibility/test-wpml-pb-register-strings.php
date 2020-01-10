dfcb7d4562195b2431f2d3c536e87bdbefc1e658
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-01-10 14:39:53 +0100

    Do not register strings if post is not handled by the current page builder
    If the post was previously edited with the page builder, the old strings
    will be automatically deleted.
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-6929

--- tests/phpunit/tests/st/compatibility/test-wpml-pb-register-strings.php
+++ tests/phpunit/tests/st/compatibility/test-wpml-pb-register-strings.php
@@ -8,8 +8,33 @@
  */
 class Test_WPML_PB_Register_Strings extends WPML_PB_TestCase2 {
 
+
+
 	/**
 	 * @test
+	 * @group wpmlcore-6929
+	 */
+	public function it_does_not_register_strings_if_page_builder_is_not_handling_post() {
+		list( , $post, $package ) = $this->get_post_and_package( 'Elementor' );
+
+		WP_Mock::expectAction( 'wpml_start_string_package_registration', $package );
+		WP_Mock::expectAction( 'wpml_delete_unused_package_strings', $package );
+
+		$data_settings = \Mockery::mock( 'IWPML_Page_Builders_Data_Settings' );
+		$data_settings->shouldReceive( 'is_handling_post' )->with( $post->ID )->andReturn( false );
+		$data_settings->shouldNotReceive( 'convert_data_to_array' );
+
+		$subject = new WPML_Concrete_Test_Register_Strings(
+			\Mockery::mock( 'IWPML_Page_Builders_Translatable_Nodes' ),
+			$data_settings,
+			\Mockery::mock( 'WPML_PB_String_Registration' )
+		);
+		$subject->register_strings( $post, $package );
+	}
+
+	/**
+	 * @test
+	 * @group wpmlcore-6929
 	 */
 	public function it_does_not_throw_error_on_invalid_data() {
 		list( $name, $post, $package ) = $this->get_post_and_package( 'Elementor' );
@@ -24,6 +49,7 @@ class Test_WPML_PB_Register_Strings extends WPML_PB_TestCase2 {
 		WP_Mock::expectAction( 'wpml_delete_unused_package_strings', $package );
 
 		$data_settings = \Mockery::mock( 'IWPML_Page_Builders_Data_Settings' );
+		$data_settings->shouldReceive( 'is_handling_post' )->with( $post->ID )->andReturn( true );
 		$data_settings->shouldReceive( 'get_meta_field' )->andReturn( '_elementor_data' );
 		$data_settings->shouldReceive( 'convert_data_to_array' )->once()->with( $invalid_json )->andReturn( null );
 
