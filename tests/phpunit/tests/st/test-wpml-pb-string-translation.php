fd66c0779a4c917d2cd679ee1bc192cc47704633
Bruce Pearson <bruce@onthegosystems.com>
2020-06-15 10:32:10 +0800

    Add test for filter

--- tests/phpunit/tests/st/test-wpml-pb-string-translation.php
+++ tests/phpunit/tests/st/test-wpml-pb-string-translation.php
@@ -16,13 +16,13 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 		list( $factory_mock, $strategy_mock, $package ) = $this->get_factory_and_strategy_mock( $string_package_id, $language_1, $language_2, $post_id );
 
 		$factory_mock->expects( $this->exactly( 2 ) )
-		        ->method( 'get_wpml_package' )
-		        ->with( $this->equalTo( $string_package_id ) )
-		        ->willReturn( $package );
+		             ->method( 'get_wpml_package' )
+		             ->with( $this->equalTo( $string_package_id ) )
+		             ->willReturn( $package );
 
 		$strategy_mock->expects( $this->exactly( $post_id ? 3 : 0 ) )
-		         ->method( 'get_package_kind' )
-		         ->willReturn( $package->kind );
+		              ->method( 'get_package_kind' )
+		              ->willReturn( $package->kind );
 
 		$wpdb_mock = $this->get_wpdb_mock( $string_package_id, $language_1, $language_2 );
 
@@ -61,28 +61,28 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 	private function get_factory_and_strategy_mock( $string_package_id, $language_1, $language_2, $post_id = null ) {
 		$package_kind = rand_str( 32 );
 
-		$factory       = $this->getMockBuilder( 'WPML_PB_Factory' )
-		                      ->setMethods( array( 'get_wpml_package' ) )
-		                      ->disableOriginalConstructor()
-		                      ->getMock();
-		$package       = $this->getMockBuilder( 'WPML_Package' )
-		                      ->setMethods()
-		                      ->disableOriginalConstructor()
-		                      ->getMock();
-		$package->kind = $package_kind;
+		$factory          = $this->getMockBuilder( 'WPML_PB_Factory' )
+		                         ->setMethods( array( 'get_wpml_package' ) )
+		                         ->disableOriginalConstructor()
+		                         ->getMock();
+		$package          = $this->getMockBuilder( 'WPML_Package' )
+		                         ->setMethods()
+		                         ->disableOriginalConstructor()
+		                         ->getMock();
+		$package->kind    = $package_kind;
 		$package->post_id = $post_id;
-		$update = $this->getMockBuilder( 'WPML_PB_Update_Post' )
-		               ->setMethods( [ 'update', 'update_content' ] )
-		               ->disableOriginalConstructor()
-		               ->getMock();
+		$update           = $this->getMockBuilder( 'WPML_PB_Update_Post' )
+		                         ->setMethods( [ 'update', 'update_content' ] )
+		                         ->disableOriginalConstructor()
+		                         ->getMock();
 		$update->expects( $post_id ? $this->once() : $this->never() )
 		       ->method( 'update' );
 
 
-		$strategy = $this->getMockBuilder( 'WPML_PB_Shortcode_Strategy' )
-		                 ->setMethods( array( 'get_package_kind', 'get_update_post' ) )
-		                 ->disableOriginalConstructor()
-		                 ->getMock();
+		$strategy     = $this->getMockBuilder( 'WPML_PB_Shortcode_Strategy' )
+		                     ->setMethods( array( 'get_package_kind', 'get_update_post' ) )
+		                     ->disableOriginalConstructor()
+		                     ->getMock();
 		$package_data = array( 'package' => $package, 'languages' => array( $language_1, $language_2 ) );
 		$strategy->method( 'get_update_post' )
 		         ->with( $this->equalTo( $package_data ) )
@@ -116,11 +116,11 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 	 * @dataProvider remove_string_db
 	 */
 	public function remove_string( $delete_count, $job_translated ) {
-		$context = rand_str();
-		$name = rand_str();
-		$job_id = mt_rand();
+		$context              = rand_str();
+		$name                 = rand_str();
+		$job_id               = mt_rand();
 		$translated_string_id = mt_rand();
-		$string_package_id = mt_rand();
+		$string_package_id    = mt_rand();
 
 		\WP_Mock::wpFunction( 'icl_unregister_string', array(
 			'times' => 1, // a string must be always unregistered contrary to a record in `icl_translate`
@@ -128,14 +128,13 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 		) );
 
 
-
 		$factory_mock = $this->getMockBuilder( 'WPML_PB_Factory' )
-		                    ->disableOriginalConstructor()
-		                    ->getMock();
-
-		$strategy_mock = $this->getMockBuilder( 'IWPML_PB_Strategy' )
 		                     ->disableOriginalConstructor()
 		                     ->getMock();
+
+		$strategy_mock = $this->getMockBuilder( 'IWPML_PB_Strategy' )
+		                      ->disableOriginalConstructor()
+		                      ->getMock();
 
 		$wpdb = $this->stubs->wpdb();
 
@@ -143,7 +142,7 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 		$wpdb->expects( $this->exactly( 2 ) )->method( 'get_var' )->willReturnOnConsecutiveCalls( $job_id, $job_translated );
 		$wpdb->expects( $this->exactly( $delete_count ) )->method( 'delete' )->with( $wpdb->prefix . 'icl_translate', array( 'field_type' => $field_type ), array( '%s' ) );
 		$pb_string_translation = new WPML_PB_String_Translation_By_Strategy( $wpdb, $factory_mock, $strategy_mock );
-		$string_data = array(
+		$string_data           = array(
 			'context'    => $context,
 			'name'       => $name,
 			'package_id' => $string_package_id,
@@ -155,14 +154,14 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 	/**
 	 * @test
 	 * @group wpmlst-1215
-	 * @link https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlst-1215
+	 * @link  https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlst-1215
 	 */
 	public function it_should_remove_string_if_job_id_is_null() {
-		$context = rand_str();
-		$name = rand_str();
-		$job_id = null;
+		$context              = rand_str();
+		$name                 = rand_str();
+		$job_id               = null;
 		$translated_string_id = mt_rand();
-		$string_package_id = mt_rand();
+		$string_package_id    = mt_rand();
 
 		\WP_Mock::wpFunction( 'icl_unregister_string', array(
 			'times' => 1,
@@ -183,7 +182,7 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 		$wpdb->expects( $this->exactly( 1 ) )->method( 'get_var' )->willReturn( $job_id );
 		$wpdb->expects( $this->exactly( 1 ) )->method( 'delete' )->with( $wpdb->prefix . 'icl_translate', array( 'field_type' => $field_type ), array( '%s' ) );
 		$pb_string_translation = new WPML_PB_String_Translation_By_Strategy( $wpdb, $factory_mock, $strategy_mock );
-		$string_data = array(
+		$string_data           = array(
 			'context'    => $context,
 			'name'       => $name,
 			'package_id' => $string_package_id,
@@ -241,6 +240,38 @@ class Test_WPML_PB_String_Translation extends WPML_PB_TestCase {
 			$updated_content,
 			$pb_string_translation->update_translations_in_content( $content, $language_1 )
 		);
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_should_allow_override_of_package_handling() {
+		$kind = 'abc';
+		$strategyKind = 'xyz';
+
+		$updater = \Mockery::mock( 'WPML_PB_XYZ_Updater' );
+		$updater->shouldReceive( 'update' )->once();
+
+		$strategy = \Mockery::mock( 'WPML_PB_Shortcode_Strategy' );
+		$strategy->shouldReceive( 'get_package_kind' )->andReturn( $strategyKind );
+		$strategy->shouldReceive( 'get_update_post' )->andReturn( $updater );
+
+		$subject = new WPML_PB_String_Translation_By_Strategy(
+			\Mockery::mock( 'wpdb' ),
+			\Mockery::mock( 'WPML_PB_Factory' ),
+			$strategy
+		);
+
+		$package = \Mockery::mock( 'WPML_Package' );
+		$package->kind = $kind;
+
+		$subject->add_package_to_update_list( $package, 'de' );
+
+		// Test that the filter can force the update
+		\WP_Mock::onFilter( 'wpml_pb_should_handle_package' )->with( false, $kind, $strategyKind )->reply( true );
+
+		$subject->save_translations_to_post();
 
 	}
 
